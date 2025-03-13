079100fedc33e71f0b63aae2063c990fd6d04462
Dhashwathi M B <dhashwathi.mb2021ai@sece.ac.in>
2025-03-13 12:30:30 +0530

    update- Added Aynsc and Await file

---
+++ async_await.js
@@ -0,0 +1,7 @@
+const getDataFromAPI = async() => {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+    const data = await response.json()
+    console.log(data)
+}
+
+getDataFromAPI()
